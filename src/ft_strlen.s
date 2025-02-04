a902c167b1f9b67176ffe80a2103ccf373c73416
rdelicad <rdelicad@gmail.com>
2025-02-04 16:09:17 +0100

    update ft_strlen

--- src/ft_strlen.s
+++ src/ft_strlen.s
@@ -1,20 +1,27 @@
-; size_t strlen(const char *s)
-; Parametes: rdi = *s
-; returns the number of bytes in the string pointed to by s
+; Function: ft_strlen
+; size_t strlen(const char *s);
+; Brief: Calculates the number of characters in a string.
+;        The count does not include the null terminator.
+; Registers:
+;   - rdi: Pointer to the beginning of the string.
+;   - rax: Used both as counter and to store the result.
+; Algorithm:
+;   Loop through each character until a null (0) is found.
+;   Then return the count in rax.
 
 section .text
     global ft_strlen
 
 ft_strlen:
-    xor     rax, rax
+    xor     rax, rax          ; Set counter to 0
 
 .loop:
-    cmp byte [rdi + rax], 0 ; Compara el caracter actual con '\0'
-    je  .end                ; Si es '\0', termina
-    inc rax                 ; Incrementa el contador en 1
-    jmp .loop               ; Repite
+    cmp byte [rdi + rax], 0   ; Check if current byte is null
+    je  .end                ; If null, exit loop
+    inc rax                 ; Increase counter
+    jmp .loop               ; Continue scanning
 
 .end:
-    ret                 ; ¡Retorna al programa!
+    ret                     ; Return to caller
 
 section .note.GNU-stack noalloc noexec nowrite progbits
